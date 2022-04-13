650b3f2e2f571043976e0f481e64232f7b49ddca
fnetodev <fneto.04@gmail.com>
2022-04-12 22:31:28 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/br/com/itsm/domain/Pessoa.java
+++ src/main/java/br/com/itsm/domain/Pessoa.java
@@ -1,21 +1,49 @@
 package br.com.itsm.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CollectionId;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.com.itsm.domain.enums.Perfil;
 
-public abstract class Pessoa {
+@Entity(name = "pessoa")
+public abstract class Pessoa implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+
+	@Column(unique = true)
 	protected String cpf;
+
+	@Column(unique = true)
+	protected String email;
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 
 	public Pessoa() {
@@ -23,11 +51,12 @@ public abstract class Pessoa {
 		addPerfis(Perfil.CLIENTE);
 	}
 
-	public Pessoa(Integer id, String nome, String cpf, String senha) {
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
+		this.email = email;
 		this.senha = senha;
 		addPerfis(Perfil.CLIENTE);
 	}
@@ -54,6 +83,14 @@ public abstract class Pessoa {
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getSenha() {
@@ -97,6 +134,4 @@ public abstract class Pessoa {
 		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
 	}
 
-	
-	
 }

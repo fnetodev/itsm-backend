2cf83b5b1e27d484fc0b879f87444696f1587811
fnetodev <fneto.04@gmail.com>
2022-04-14 18:21:34 -0300

    protegendo aplicacao contra serializacao

--- src/main/java/br/com/itsm/domain/Tecnico.java
+++ src/main/java/br/com/itsm/domain/Tecnico.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import br.com.itsm.domain.enums.Perfil;
 
 @Entity(name = "tecnico")
@@ -13,6 +15,7 @@ public class Tecnico extends Pessoa {
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 

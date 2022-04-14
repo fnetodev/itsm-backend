138b7aeae28e582806b94ffe0b61271a0d92f725
fnetodev <fneto.04@gmail.com>
2022-04-14 18:23:51 -0300

    endpoint findById para tecnico

--- src/main/java/br/com/itsm/repository/TecnicoRepository.java
+++ src/main/java/br/com/itsm/repository/TecnicoRepository.java
@@ -2,9 +2,9 @@ package br.com.itsm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.itsm.domain.Pessoa;
+import br.com.itsm.domain.Tecnico;
 
-public interface TecnicoRepository extends JpaRepository<Pessoa, Integer>{
+public interface TecnicoRepository extends JpaRepository<Tecnico, Integer>{
 
 	
 	
--- src/main/java/br/com/itsm/services/DBService.java
+++ src/main/java/br/com/itsm/services/DBService.java
@@ -28,14 +28,23 @@ public class DBService {
 	public void iniciaDB() {
 		Tecnico tec1 = new Tecnico(null, "Francisco", "41323315632", "francisco@email.com", "123");
 		tec1.addPerfis(Perfil.ADMIN);
+		Tecnico tec2 = new Tecnico(null, "Tecnico01", "45558622601", "tec1@email.com", "123");
+		Tecnico tec3 = new Tecnico(null, "Tecnico02", "80828198209", "tec2@email.com", "123");
+		Tecnico tec4 = new Tecnico(null, "Tecnico03", "52622813899", "tec3@email.com", "123");
 		
-		Cliente cli1 = new Cliente(null, "Linus Torvals", "58495648539", "linus@email.com", "123");
+		Cliente cli1 = new Cliente(null, "Cliente1", "48433273108", "cliente1@email.com", "123");
+		Cliente cli2 = new Cliente(null, "Cliente2", "83686812002", "cliente2@email.com", "123");
+		Cliente cli3 = new Cliente(null, "Cliente3", "04494853151", "cliente3@email.com", "123");
+		Cliente cli4 = new Cliente(null, "Cliente4", "77367732110", "cliente4@email.com", "123");
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01","Primeiro chamado.", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 02","Segundo chamado.", tec2, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.ALTA, Status.ENCERRADO, "Chamado 03","Terceiro chamado.", tec3, cli3);
+		Chamado c4 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 04","Quarto chamado.", tec4, cli4);
+		
 	
-	
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
+		tecnicoRepository.saveAll(Arrays.asList(tec1,tec2,tec3,tec4));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2,cli3,cli4));
+		chamadoRepository.saveAll(Arrays.asList(c1,c2,c3,c4));
 	}
 }

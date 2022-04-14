b0443e30576a13cab156581e08bd5a19eee2f7c2
fnetodev <fneto.04@gmail.com>
2022-04-14 16:39:41 -0300

    perfil de desenvovimento

---
+++ src/main/java/br/com/itsm/config/DevConfig.java
@@ -0,0 +1,28 @@
+package br.com.itsm.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.itsm.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		if(value.equals("create") || value.equals("update")) {
+			this.dbService.iniciaDB();
+		}
+		return false;
+	}
+}

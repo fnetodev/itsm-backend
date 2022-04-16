e888fdb50083db50e7441f7b0613512620633db5
fnetodev <fneto.04@gmail.com>
2022-04-15 23:22:01 -0300

    padrao DTO para tecnico

--- src/main/java/br/com/itsm/controller/TecnicoController.java
+++ src/main/java/br/com/itsm/controller/TecnicoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.itsm.domain.Tecnico;
+import br.com.itsm.domain.dto.TecnicoDTO;
 import br.com.itsm.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoController {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico tecnico = service.findById(id);
-		return ResponseEntity.ok().body(tecnico);
+		return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
 	}
 
 }

d8a44c6b24314b636650f520c8caadbbd79631d1
fnetodev <fneto.04@gmail.com>
2022-04-16 00:17:29 -0300

    Excecao para objeto nao encontrado

--- src/main/java/br/com/itsm/services/TecnicoService.java
+++ src/main/java/br/com/itsm/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.itsm.domain.Tecnico;
 import br.com.itsm.repository.TecnicoRepository;
+import br.com.itsm.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id ));
 
 	}
 }
